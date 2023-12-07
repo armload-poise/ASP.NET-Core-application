2d6514a5a767f3b9836fcacaf6185c41162657f8
Liam Smith <153170096+armload-poise@users.noreply.github.com>
2023-12-07 14:57:04 +0700

    Create ApplicationDbContext.cs

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+// Models/ApplicationDbContext.cs
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineShop.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
