4e25a333a19cdab864d0dba18aab8e18f78b3ce8
Liam Smith <153170096+armload-poise@users.noreply.github.com>
2023-12-07 14:57:15 +0700

    Create Product.cs

---
+++ Product.cs
@@ -0,0 +1,10 @@
+// Models/Product.cs
+namespace OnlineShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
